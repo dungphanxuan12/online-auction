be069e92f2791e21c03fd05f2572d223f359c743
dungphanxuan12 <dungphanxuan12@gmail.com>
2020-08-28 23:54:09 +0700

    initial commit

---
+++ src/main/java/com/auction/products_auction/ProductsAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.auction.products_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auction/products_auction/ProductsAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.products_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
